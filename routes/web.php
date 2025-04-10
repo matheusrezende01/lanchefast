f83dc816b04d94aada47cb0baa3e747f0f628bd6
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2025-04-10 12:58:15 -0300

    novas funcionalidades

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('clientes')->group(function () {
+    Route::get('/', \App\Livewire\Clientes\Index::class)->name('clientes.index');
+    Route::get('/create', \App\Livewire\Clientes\Create::class)->name('clientes.create');
+    Route::get('/{cliente}', \App\Livewire\Clientes\Show::class)->name('clientes.show');
+    Route::get('/{cliente}/edit', \App\Livewire\Clientes\Edit::class)->name('clientes.edit');
 });
